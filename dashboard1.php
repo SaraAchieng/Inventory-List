328a2cdcc1dcd1cc1923415386147306c35920f4
Mungai <111287651+Thornytaste@users.noreply.github.com>
2023-02-07 20:55:35 +0300

    Update dashboard1.php

--- dashboard1.php
+++ dashboard1.php
@@ -9,4 +9,5 @@
 
 <h1>Welcome <?php echo ucfirst($_SESSION['first_name']); ?></h1>
 <p>This is your dashboard</p>
+<a href="items.php?items=true">Items</a>
 <a href="logout.php?logout=true">Logout</a>
