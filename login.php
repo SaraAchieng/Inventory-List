1e9d834cfcccc41640c4aac5f272111153c0f100
thornytaste <mungaiian12@gmail.com>
2023-02-09 12:43:13 +0300

    changes in login

--- login.php
+++ login.php
@@ -32,34 +32,3 @@
 		}
 
 ?>
-<!doctype html>
-<html>
-<head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" >
-
-</head>
-<body class="bg-dark">
-
-<div class="container h-100">
-	<div class="row h-100 mt-5 justify-content-center align-items-center">
-		<div class="col-md-5 mt-5 pt-2 pb-5 align-self-center border bg-light">
-			<h1 class="mx-auto w-25" >Login</h1>
-			<form method="POST" action="">
-				<div class="form-group">
-					<label for="email">Email:</label>
-					<input type="text" name="email" placeholder="Enter Email" class="form-control">
-				</div>
-				<div class="form-group">
-				<label for="email">Password:</label>
-					<input type="password" name="password" placeholder="Enter Password" class="form-control">
-				</div>
-
-				<button type="submit" name="submit" class="btn btn-primary">Submit</button>
-				
-				<a href="registration.php" class="btn btn-primary">Register</a>
-			</form>
-		</div>
-	</div>
-</div>
-</body>
-</html>
